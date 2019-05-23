c1383fb1223c5d036fcfa73567b07e8bcef6f2ba
Glitch (hello-express) <none>
2019-05-23 17:15:06 +0000

    👧💈 Checkpoint ./server.js:4409275/483

--- server.js
+++ server.js
@@ -36,19 +36,20 @@ app.get('/messages', function(request, response) {
   response.json(messages);
 });
 
-app.get('/update', function(request, response) {
+app.get('/updateName', function(request, response) {
   
   // read id, from and text in from the query strings i.e request.query.id
   const msgId = request.query.id;
   const msgName = request.query.name;
-  const msgText = request.query.text;
   
   
   // if from exists, update messages[i].from
   
 response.json(messages.map(msg=>{
-  if (msg.id === msgId && msgName){
-    return ({...msg, from: msgText})
+  if (msg.id.toString() === msgId) {
+    return ({...msg, from: msgName})
+  } else {
+    return msg
   }
 }))
   // if text exists, update messages[i].text
